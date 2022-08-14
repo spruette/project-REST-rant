1115d735b4ed9899119db5bea1624608031f1f1e
unknown <scotteychaos@gmail.com>
2022-08-14 11:02:32 -0400

    Navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
           <main>
               <h1>HOME</h1>
               <div>
-                <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                <img src="/images/chia-fruit-drink.jpg"  alt="Chia Fruit Shake"/>
               <div>
                 Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
               </div>
