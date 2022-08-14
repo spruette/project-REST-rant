3c4c40c99d2825e57ec34d37a152166978263592
unknown <scotteychaos@gmail.com>
2022-08-14 11:07:36 -0400

    Show route connected

--- Controllers/places.js
+++ Controllers/places.js
@@ -39,4 +39,18 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id]})
+  }
+})
+
+
 module.exports = router
